8b2aa87c3b768baa76d7a326ea4c91f61967fa73
Omar-Walid-MD <omar.madinah49@yahoo.com>
2022-10-20 14:42:21 +0200

    Added functional user activity tabs

--- src/UserActivityPage.js
+++ src/UserActivityPage.js
@@ -2,6 +2,7 @@ import {useState, useEffect} from "react"
 import { Link } from "react-router-dom";
 import Header from "./Header";
 import Post from "./Post";
+import Comment from "./Comment";
 import './MainPage.css';
 import "./UserActivityPage.css"
 
@@ -10,15 +11,50 @@ function UserActivityPage({posts, currentUser, setCurrentUser})
 
     const [currentTab,setCurrentTab] = useState("overview");
 
+    const [comments,setComments] = useState();
+
     function handleCurrentTab(event)
     {
         setCurrentTab(event.target.name)
     }
 
-    function sortPosts(postList)
+    function GetContent(currentTab)
     {
-        return postList.sort().slice().reverse();
+      let contents = null;
+      
+      if(currentTab==="overview")
+      {
+        contents = [...posts.map((post)=>({...post,type: "post"})),...comments.map((comment)=>({...comment,type: "comment"}))];
+      }
+      else if(currentTab==="posts")
+      {
+        contents = posts.map((post)=>({...post,type: "post"}));
+      }
+      else if(currentTab==="comments")
+      {
+        contents = comments.map((comment)=>({...comment,type: "comment"}));
+      }
+
+      return contents.filter((content)=>content.user===currentUser.username);
     }
+
+    function SortContent(contents)
+    {
+        return contents.sort().slice().reverse();
+    }
+
+    useEffect(()=>{
+
+      fetch('http://localhost:8000/comments')
+      .then(res => {
+        return res.json()
+      })
+      .then((data)=>{
+      //   console.log(data);
+      setComments(data);
+      })
+  
+    },[posts]);
 
     return (
         <div className="main-page">
@@ -30,11 +66,15 @@ function UserActivityPage({posts, currentUser, setCurrentUser})
                 <button className="activity-page-option-button" name="posts" current={currentTab === "posts" ? "true" : "false"} onClick={handleCurrentTab} >Posts</button>
                 <button className="activity-page-option-button" name="comments" current={currentTab === "comments" ? "true" : "false"} onClick={handleCurrentTab} >Comments</button>
               </div>
+              <div className="activity-page-content-container">
               {
-                posts && posts.length>0 ? sortPosts(posts).map((post)=>
-                  <Post post={post} currentUser={currentUser} setCurrentUser={setCurrentUser} key={"post"+post.id} />
-                ) : <div className="blog-empty-label flex-center"><h1>No Posts Available</h1></div>
+                posts && SortContent(GetContent(currentTab)).map((content)=>
+                 
+                   content.type==="post" ? <Post post={content} key={content.id} /> : content.type==="comment" && <Comment comment={content} key={content.id}/>
+                 
+                 )
               }
+              </div>
             </div>
           </div>
         </div>
