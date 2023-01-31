3d129e55d506adf5f8595f5eaae47c2fdf0ad459
Omar-Walid-MD <omar.madinah49@yahoo.com>
2023-01-31 22:53:18 +0200

    Added password visibility toggle for password inputs

--- src/Main Page/TextInput.js
+++ src/Main Page/TextInput.js
@@ -1,21 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./TextInput.css";
 
 
 function TextInput({containerType,inputType="text",selectorClass,inputLabel,inputName="",inputValue,inputFunc,maxLength=0,required,minheight=0,onInput})
 {
-    // useEffect(()=>{
-    //     console.log(inputValue==="");
-    // })
+
+    const [passwordVisible,setPasswordVisible] = useState(false);
+
+    function handlePasswordVisible()
+    {
+        setPasswordVisible(prev => !prev);
+    }
+
     return (
         <div className={"text-input-container " + selectorClass}>
             {
                 containerType==="field" ?
-                <input className="text-input text-input-field" type={inputType} name={inputName} value={inputValue} required={required} onChange={inputFunc} autoComplete/>
+                <input className="text-input text-input-field" type={inputType==="password" ? passwordVisible ? "text" : "password" : inputType} name={inputName} value={inputValue} required={required} onChange={inputFunc} autoComplete/>
                 : containerType==="area" &&
-                <textarea className="text-input text-input-area" type={inputType} minheight={minheight} value={inputValue} onChange={inputFunc} onInput={onInput}></textarea>
+                <textarea className="text-input text-input-area" type={inputType==="password" ? passwordVisible ? "text" : "password" : inputType} minheight={minheight} value={inputValue} onChange={inputFunc} onInput={onInput}></textarea>
             }
             <div className="text-input-label">{inputLabel}</div>
+            {
+                inputType==="password" &&
+                <button className="text-input-toggle-password-button flex-center" type="button" onClick={handlePasswordVisible}>
+                    {
+                        passwordVisible ?
+                        <i class='bx bx-show'></i>
+                        : <i class='bx bx-hide'></i>
+                    }
+                </button>
+            }
         </div>
     )
 }
